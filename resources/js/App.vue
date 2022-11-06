ccf7d7e8d9dc2267c67c44d1569e29823e7e9525
Taro Tanaka <tarotanaka@example.com>
2022-11-06 11:16:26 +0900

    [update] Vue3 を使用

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Counter from "./components/Counter.vue";
+import logo from "../images/logo-with-shadow.png";
+</script>
+
+<template>
+    <h1>Hello World</h1>
+    <img :src="logo" />
+    <img src="../images/logo-with-shadow.png" />
+    <Counter />
+</template>
