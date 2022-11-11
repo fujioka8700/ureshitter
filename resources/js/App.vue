ae119c52000aadc17b42224d6693f7528c426f95
Taro Tanaka <tarotanaka@example.com>
2022-11-11 15:21:17 +0900

    [add] ESLint 設定追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,12 +4,12 @@ import logo from "../images/logo-with-shadow.png";
 </script>
 
 <template>
-    <h1>Hello World</h1>
-    <img :src="logo" />
-    <img src="../images/logo-with-shadow.png" />
-    <Counter />
-    <p class="text-primary">Hello</p>
-    <button type="button" class="btn btn-primary">ボタン</button>
-    <button type="button" class="btn btn-primary rounded-pill">長丸ボタン</button>
-    <p class="text-success">Hello</p>
+  <h1>Hello World</h1>
+  <img :src="logo" />
+  <img src="../images/logo-with-shadow.png" />
+  <Counter />
+  <p class="text-primary">Hello</p>
+  <button type="button" class="btn btn-primary">ボタン</button>
+  <button type="button" class="btn btn-primary rounded-pill">長丸ボタン</button>
+  <p class="text-success">Hello</p>
 </template>
