0e3042ebaef9ecdae19cc8a5b7b4744d443a23f0
Taro Tanaka <tarotanaka@example.com>
2022-11-13 14:42:19 +0900

    [update] Popper install

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import EmotionForm from './components/EmotionForm.vue';
+
+import UsuallyHappy from '../images/usually-happy.svg';
 </script>
 
 <template>
   <TheHeader />
   <EmotionForm />
+
+  <article>
+    <img :src="UsuallyHappy" alt="" />
+  </article>
 </template>
