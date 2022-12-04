8a836f6d028688271d82bcc2c2e345475261f88d
Fujioka <tarotanaka@example.com>
2022-12-04 17:13:39 +0900

    [clean] EmotionForm.vueのpostMessage()のリファクタリングしました。

--- resources/js/components/modules/ArticleItem.vue
+++ resources/js/components/modules/ArticleItem.vue
@@ -59,6 +59,8 @@ export default {
         default:
           break;
       }
+
+      return false;
     },
   },
 };
--- resources/js/components/pages/TheSuccess.vue
+++ resources/js/components/pages/TheSuccess.vue
@@ -73,6 +73,8 @@ export default {
         default:
           break;
       }
+
+      return false;
     },
   },
 };
