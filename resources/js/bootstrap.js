71474b17e98cdbabe9b8008ecf8588d499ccbc3b
Fujioka <tarotanaka@example.com>
2022-12-17 21:47:28 +0900

    [update] CSRF対策を行った。

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,10 +7,19 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+import { getCookieValue } from './util';
 import axios from 'axios';
 window.axios = axios;
 
+// Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.request.use((config) => {
+  // クッキーからトークンを取り出してヘッダーに添付する
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN');
+
+  return config;
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
