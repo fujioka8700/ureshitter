3ddc6718a9b03637f595ce6e9623fac119fa80d7
Fujioka <tarotanaka@example.com>
2022-12-18 14:02:14 +0900

    [fix] 投稿されたメッセージ一覧の、アイコンの背景を青色3色に変更した。

--- resources/js/components/modules/ArticleItem.vue
+++ resources/js/components/modules/ArticleItem.vue
@@ -8,7 +8,7 @@ import VeryHappy from '../../../images/very-happy.svg';
   <article class="c-article">
     <div class="container">
       <div class="article-item mt-3 d-flex">
-        <img :src="imgSrc" class="bg-secondary bg-opacity-25 rounded-1 me-2" alt="" />
+        <img :src="imgSrc" class="bg-opacity-25 rounded-1 me-2" :class="bgColor" alt="" />
         <div>
           <p class="mb-0">{{ name }} さん</p>
           <div class="article-message overflow-auto text-break">
@@ -25,6 +25,7 @@ export default {
   data() {
     return {
       imgSrc: '',
+      bgColor: '',
     };
   },
   props: {
@@ -49,12 +50,15 @@ export default {
       switch (this.emotion) {
         case 0:
           this.imgSrc = LittleHappy;
+          this.bgColor = 'u-bg-little-happy';
           break;
         case 1:
           this.imgSrc = UsuallyHappy;
+          this.bgColor = 'u-bg-usually-happy';
           break;
         case 2:
           this.imgSrc = VeryHappy;
+          this.bgColor = 'u-bg-very-happy';
           break;
         default:
           break;
