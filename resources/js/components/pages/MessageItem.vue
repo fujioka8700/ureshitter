251500b0206a0c0312df01bbf5e5e68733f27b59
Fujioka <tarotanaka@example.com>
2022-12-19 17:10:51 +0900

    [update] メッセージ1つのページ作成

---
+++ resources/js/components/pages/MessageItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-message-item">
+    <div class="item-title">
+      <p>MessageItem</p>
+      <p>{{ id }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  created() {
+    console.log(this.id);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.p-message-item {
+  .item-title {
+    padding-top: 60px;
+  }
+}
+</style>
