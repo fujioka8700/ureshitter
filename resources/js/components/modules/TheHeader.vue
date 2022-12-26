11fc57de4fd0c6f9aa5fb3696985a5e2e8305961
Fujioka <tarotanaka@example.com>
2022-12-26 20:18:26 +0900

    [update] PC版のnavを、スマホでは非表示にした。

--- resources/js/components/modules/TheHeader.vue
+++ resources/js/components/modules/TheHeader.vue
@@ -15,34 +15,41 @@ import logo from '../../../images/logo.svg';
             <h1 class="header-logo ms-1 text-black">Ureshitter</h1>
           </router-link>
         </div>
-        <div>
-          <ul class="nav">
-            <li class="nav-item">
-              <router-link :to="{ name: 'about' }" class="nav-link nav-link__color">
-                Ureshitterとは
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link nav-link__color"
-                href="https://twitter.com/fujioka8700"
-                target="_blank"
-              >
-                twitter
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link nav-link__color"
-                href="https://github.com/fujioka8700/ureshitter"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
-        </div>
+        <nav>
+          <div class="d-none d-sm-block">
+            <ul class="nav">
+              <li class="nav-item">
+                <router-link :to="{ name: 'about' }" class="nav-link nav-link__color">
+                  Ureshitterとは
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link nav-link__color"
+                  href="https://twitter.com/fujioka8700"
+                  target="_blank"
+                >
+                  twitter
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link nav-link__color"
+                  href="https://github.com/fujioka8700/ureshitter"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="d-block d-sm-none">
+            <!-- ここにハンバーガーメニュー -->
+          </div>
+        </nav>
       </div>
     </div>
   </header>
 </template>
+
+<style lang="scss"></style>
